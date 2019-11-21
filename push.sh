9c6c1f53539f31f91a6449a536ac6176ed6abe92
StayZeal <543294760@qq.com>
2019-11-21 14:24:37 +0800

    default commit msg

---
+++ push.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+git config user.name "StayZeal"
+git config user.email "543294760@qq.com"
+git add .
+commit(){
+  str="$1"
+  #echo "$1"
+  echo "开始提交：$str"
+  if [ -z "$str" ]; then
+     str="default commit msg"
+  #else
+  #  str="$1"
+  fi
+  git commit -m"$str"
+  echo "提交信息：$str"
+}
+commit "$1"
+git push
+#打开网页
+https://github.com/StayZeal/ToolShell
